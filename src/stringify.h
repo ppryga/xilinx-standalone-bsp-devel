f1dd47282b5e7c4de1c134cf258858fab3eb89e1
Piotr Pryga <pryga.piotr@outlook.com>
2022-11-07 00:05:27 +0100

    stringify: Add header with stringification macro
    Signed-off-by: Piotr Pryga <pryga.piotr@outlook.com>

---
+++ src/stringify.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+* Copyright (c) 2022 Piotr Pryga - All rights reserved.
+*
+* Author: Piotr Pryga <pryga.piotr@outlook.com>
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+/*****************************************************************************/
+/**
+* @file stringify.h
+*
+* This file provides macro to stringify.
+*******************************************************************************/
+
+#ifndef STRINGIFY_H
+
+#define STRINGIFY_1(x)  #x
+#define STRINGIFY(x)    STRINGIFY_1(x)
+
+#endif /* STRINGIFY_H */
