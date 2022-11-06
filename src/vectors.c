3b7045cecc28fe65eed1f98bf66e0109dade1616
Piotr Pryga <pryga.piotr@outlook.com>
2022-11-07 00:19:58 +0100

    vectors: temp - started re-work of exceptions handling vectors
    Signed-off-by: Piotr Pryga <pryga.piotr@outlook.com>

--- src/vectors.c
+++ src/vectors.c
@@ -29,6 +29,8 @@
 
 #include "xil_exception.h"
 #include "vectors.h"
+#include "asm_sysreg.h"
+#include "esr.h"
 
 /************************** Constant Definitions *****************************/
 
@@ -144,6 +146,70 @@ void PrefetchAbortInterrupt(void)
 ******************************************************************************/
 void SynchronousInterrupt(void)
 {
+	uint64_t esr = SYSREG_READ(esr_el1);
+
+	switch(esr_elx_ec_value_get(esr)) {
+	case ESR_ELX_EC_UNKNOWN:
+	case ESR_ELX_EC_WFX:
+	/* Unallocated 0b000010 (0x2) */
+	case ESR_ELX_EC_CP15_MR:
+	case ESR_ELX_EC_CP15_MRR:
+	case ESR_ELX_EC_CP14_MR:
+	case ESR_ELX_EC_CP14_LS:
+	case ESR_ELX_EC_ASIMD:
+	case ESR_ELX_EC_CP10_ID:
+	case ESR_ELX_EC_PAC:
+	case ESR_ELX_EC_LS64:
+	/* Unallocated 0b001011 (0xB)*/
+	case ESR_ELX_EC_CP14_MRR:
+	case ESR_ELX_EC_BTI:
+	case ESR_ELX_EC_ILL:
+	/* Unallocated 0b001111 - 0b010000 (0xF - 0x10) */
+	case ESR_ELX_EC_SVC_32:
+	case ESR_ELX_EC_HVC_32:
+	case ESR_ELX_EC_SMC_32:
+	/* Unallocated 0b010100 (0x14) */
+	case ESR_ELX_EC_SVC_64:
+	case ESR_ELX_EC_HVC_64:
+	case ESR_ELX_EC_SMC_64:
+	case ESR_ELX_EC_SYS_64:
+	case ESR_ELX_EC_SVE_64:
+	case ESR_ELX_EC_ERET:
+	/* Unallocated 0b011011 (0x1B) */
+	case ESR_ELX_EC_FPAC:
+	case ESR_ELX_EC_SME:
+	/* Unallocated 0b011110 (0x1E)*/
+	case ESR_ELX_EC_IMP_DEF:
+	case ESR_ELX_EC_IABT_LOW:
+	case ESR_ELX_EC_IABT_CUR:
+	case ESR_ELX_EC_PC_ALIGN:
+	/* Unallocated 0b100011 (0x23) */
+	case ESR_ELX_EC_DABT_LOW:
+	case ESR_ELX_EC_DABT_CUR:
+	case ESR_ELX_EC_SP_ALIGN:
+	case ESR_ELX_EC_MOPS:
+	case ESR_ELX_EC_FP_32:
+	/* Unallocated 0b101001 - 0b101011 (0x29 - 0x2B) */
+	case ESR_ELX_EC_FP_64:
+	/* Unallocated 0b101101 - 0b101110 (0x2D - 0x2E) */
+	case ESR_ELX_EC_SERROR:
+	case ESR_ELx_EC_BREAKPT_LOW:
+	case ESR_ELx_EC_BREAKPT_CUR:
+	case ESR_ELx_EC_SW_STEP_LOW:
+	case ESR_ELx_EC_SW_STEP_CUR:
+	case ESR_ELx_EC_WATCHPT_LOW:
+	case ESR_ELx_EC_WATCHPT_CUR:
+	/* Unallocated: 0b110110 - 0b110111 (0x36 - 0x37) */
+	case ESR_ELx_EC_BKPT32:
+	/* Unallocated: 0b1101001 (0x39) */
+	case ESR_ELx_EC_VECTOR32:
+	/* Unallocated: 0b111011 (0x3B) */
+	case ESR_ELx_EC_BRK64:
+	/* Unallocated: 0b111101 - 0b111111 (0x3D - 0x3F) */
+	case ESR_ELx_EC_MAX:
+		break;
+	}
+
 	XExc_VectorTable[XIL_EXCEPTION_ID_SYNC_INT].Handler(XExc_VectorTable[
 					XIL_EXCEPTION_ID_SYNC_INT].Data);
 }
